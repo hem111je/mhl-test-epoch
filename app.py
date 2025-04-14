0721720b949d4e65ce74ff3142fecac5dd281028
Hemtman Jenna <s2400619@edu.bc.fi>
2025-04-14 20:39:48 +0300

    test

--- app.py
+++ app.py
@@ -9,5 +9,23 @@ def seconds_since_epoch():
 
 @app.route("/")
 def seconds():
-    return seconds_since_epoch()
+    return str(seconds_since_epoch())  # Convert the integer to a string
 
+@app.route("/hours")
+def hours():
+    seconds = seconds_since_epoch()
+    hours = seconds // 3600
+    return str(hours)
+
+@app.route("/minutes")
+def minutes():
+    seconds = seconds_since_epoch()
+    minutes = seconds // 60
+    return str(minutes)
+
+#and this route returns weeks since epoch
+@app.route("/weeks")
+def weeks():
+    seconds = seconds_since_epoch()
+    weeks = seconds // (3600 * 24 * 7)
+    return str(weeks)
